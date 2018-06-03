49edab7b4f567918c0f9094b68bdb3aba6a5b0ca
Thomas Sileghem <th.sileghem@gmail.com>
2018-06-03 19:06:29 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 const path = require("path");
 const DynamicCdnWebpackPlugin = require("dynamic-cdn-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Repro case",
-            template: path.resolve(__dirname, "index.html"),
-            inlineSource: '.js$'
-        }),
-        // new HtmlWebpackInlineSourcePlugin(),
-        new DynamicCdnWebpackPlugin({
-            env: "production"
-        })
-    ]
-}
+mode: "development",
+	entry: { bundle: "./src/index.js" },
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/"
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Repro case",
+			template: path.resolve(__dirname, "index.html"),
+			inlineSource: "^/"
+		}),
+		new HtmlWebpackInlineSourcePlugin(),
+		new DynamicCdnWebpackPlugin({
+			env: "production"
+		})
+	]
+};
